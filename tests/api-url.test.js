04f25f3792232e6edb8deac15b54fcd54fa4aa99
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-11-07 21:55:12 +0100

    test: write unittests

---
+++ tests/api-url.test.js
@@ -0,0 +1,15 @@
+const getUrl = require('../src/utils/api-url');
+
+describe('getUrl', () => {
+    process.env.LASTFM_API_KEY = "LASTFM_API_KEY";
+    it('should return the host of the api', () => {
+        ({ host, requestUrl } = getUrl("John Doe"));
+        expect(host).toEqual('ws.audioscrobbler.com/2.0/');
+
+    });
+    it('should return the url request of the api', () => {    
+        ({ host, requestUrl } = getUrl("John Doe"));
+        expect(requestUrl).toEqual('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=John%20Doe&api_key=LASTFM_API_KEY&limit=1&format=json');
+    });
+}
+);
